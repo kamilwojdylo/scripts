ec4c6cadccdeadae156cbe2f5ab9e720e666dd83
Kamil Wojdyło <kamil.wojdylo@wp.pl>
2016-10-06 08:13:04 +0200

    Adds aptitude - package manager. Creates file with settings.

---
+++ desktop-settings.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#GIT
+git config --global user.name "Kamil Wojdyło"
+git config --global user.email "kamil.wojdylo@wp.pl"
--- desktop.sh
+++ desktop.sh
@@ -1,3 +1,21 @@
-!#/bin/bash
+#!/bin/bash
 
-apt-get install -y xorg i3 i3blocks intel-microcode i965-va-driver git fonts-hack-ttf fonts-font-awesome pavucontrol xbindkeys chromium-l10n treesheets compton curl mpv neovim feh
+apt-get install -y \
+	xorg \
+	i3 \
+	intel-microcode \
+	i965-va-driver \
+	git \
+	fonts-hack-ttf \
+	fonts-font-awesome \
+	pavucontrol \
+	xbindkeys \
+	chromium-l10n \
+	treesheets \
+	compton \
+	curl \
+	mpv \
+	neovim \
+	feh \
+	aptitude
+
