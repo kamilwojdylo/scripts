2e5d9dcd271a797b961f1b7dcec5e193c9cf3263
Kamil Wojdyło <kamil.wojdylo@wp.pl>
2016-10-07 16:26:06 +0200

    Replaces tab with spaces.

--- desktop.sh
+++ desktop.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 apt-get install -y \
-	xorg \
-	i3 \
-	i3blocks \
-	intel-microcode \
-	i965-va-driver \
-	git \
-	fonts-hack-ttf \
-	fonts-font-awesome \
-	pavucontrol \
-	xbindkeys \
-	chromium-l10n \
-	treesheets \
-	compton \
-	curl \
-	mpv \
-	neovim \
+    xorg \
+    i3 \
+    i3blocks \
+    intel-microcode \
+    i965-va-driver \
+    git \
+    fonts-hack-ttf \
+    fonts-font-awesome \
+    pavucontrol \
+    xbindkeys \
+    chromium-l10n \
+    treesheets \
+    compton \
+    curl \
+    mpv \
+    neovim \
     python-pip \
     python3-pip \
-	feh \
-	aptitude \
-	cups \
-	hplip \
-	hplip-gui \
-	okular \
-	vlc \
-	jmtpfs \
-	unzip
+    feh \
+    aptitude \
+    cups \
+    hplip \
+    hplip-gui \
+    okular \
+    vlc \
+    jmtpfs \
+    unzip
